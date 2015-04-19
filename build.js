aa3efdc52130bc02cd30cae2344d64c1cfbbb0c6
Jamie Czerwinski <jamie.czerwinski@gmail.com>
2015-04-19 15:24:08 -0600

    Set up draft test/deploy scripts

--- build.js
+++ build.js
@@ -27,10 +27,10 @@ metalsmith(__dirname)
 	// 	relative: false
 	// }))
 	.use(path())
-	.use(function (files, smith, done) {
-		console.log(files)
-		done()
-	})
+	// .use(function (files, smith, done) {
+	// 	console.log(files)
+	// 	done()
+	// })
 	.use(layouts({ // Before articles, otherwise the whole article page, header/footer and all, gets injected to index.
 		engine: 'jade',
 		pattern: '{index.html, feed.xml, archive/index.html}'
@@ -40,8 +40,6 @@ metalsmith(__dirname)
 		pattern: 'articles/**/*.html',
 		default: 'article.jade'
 	}))
-	.use(serve())
-	.use(watch())
 	.build(function (err) {
 		if (err) throw err;
 	});
--- serve.js
+++ serve.js
@@ -1,14 +1,47 @@
 var metalsmith = require('metalsmith');
-var watch = require('metalsmith-watch');
+var markdown = require('metalsmith-markdown');
+var layouts = require('metalsmith-layouts');
+var collections = require('metalsmith-collections');
 var serve = require('metalsmith-serve');
+var watch = require('metalsmith-watch');
+var placeholder = require('metalsmith-placeholder');
+var permalinks = require('metalsmith-permalinks');
+var path = require('metalsmith-path');
+
+var metadata = require('./metadata.js');
+
+// Build posts
 metalsmith(__dirname)
-/*	.use(watch({
-		livereload: 35728
-	}))*/
-	.use(serve({
-		verbose: true
-	})).build();
-// Watch -> build, livereload
-
-
-// connect, livereload
+	.metadata(metadata)
+	.use(collections({
+		articles: {
+			pattern: 'articles/**/*.md',
+			sortBy: 'date',
+			reverse: true
+		}
+	}))
+	.use(markdown())
+	.use(placeholder())
+	 // Must be after markdown, placeholder. Path seems to get clobbered otherwise.
+	// .use(permalinks({
+	// 	relative: false
+	// }))
+	.use(path())
+	.use(function (files, smith, done) {
+		console.log(files)
+		done()
+	})
+	.use(layouts({ // Before articles, otherwise the whole article page, header/footer and all, gets injected to index.
+		engine: 'jade',
+		pattern: '{index.html, feed.xml, archive/index.html}'
+	}))
+	.use(layouts({
+		engine: 'jade',
+		pattern: 'articles/**/*.html',
+		default: 'article.jade'
+	}))
+	.use(serve())
+	.use(watch())
+	.build(function (err) {
+		if (err) throw err;
+	});
